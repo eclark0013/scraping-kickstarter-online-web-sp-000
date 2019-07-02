bf67a1b89cda5401e2d6a47d3fc76adce867ac62
Eric Clark <eclark0013@gmail.com>
2019-07-02 19:03:05 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -11,7 +11,7 @@ def create_project_hash
 
   kickstarter.css("li.projects.grid_4").each do |project|
     title = project.css("h2.bbcard_name strong a").text
-    projects[title.to_sym] = {
+    projects[:title] = {
       :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
       :description => project.css("p.bbcard_blurb").text,
       :location => project.css("ul.project-meta span.location-name").text,
