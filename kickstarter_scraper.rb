a19907b971755de8c8da711b943cbea89eb697fe
Eric Clark <eclark0013@gmail.com>
2019-07-02 18:27:03 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,10 +1,16 @@
 # require libraries/modules here
-require "nokogiri"
+require 'nokogiri'
+require 'pry'
 
 def create_project_hash
   # write your code here
   html = File.read('fixtures/kickstarter.html')
 
   kickstarter = Nokogiri::HTML(html)
-  
+
 end
+
+
+create_project_hash
+
+binding.pry
